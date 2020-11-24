0aec921b4c2c48167ba9556ff8cd25471fa1fb20
saew <saew0163@gmail.com>
2020-11-24 10:52:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a / b);
+        }
+    }
+}
